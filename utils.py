717a2e17c32c833eeb91d3a5b4f215ea4ae1a037
Ocelot777 <373933165@qq.com>
2019-11-05 23:06:58 +0800

    add AverageMeter

--- utils.py
+++ utils.py
@@ -1,6 +1,24 @@
 import string
 
 
+class AverageMeter(object):
+    '''compute and store the average and current value'''
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0.
+        self.avg = 0.
+        self.sum = 0.
+        self.count = 0.
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += self.val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
 def get_dict(dict_type: str, voc_type: str,  EOS='EOS', PADDING='PADDING', UNKNOWN='UNKNOWN'):
     vocabulary = [EOS, PADDING, UNKNOWN]
     if voc_type.upper() == 'LOWER_CASE':
